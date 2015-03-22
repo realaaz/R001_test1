e6eff4fefca85ff57bb0fa5648d5a7470ae3886b
realaaz <ivnzak@gmail.com>
2015-03-22 19:42:26 +0200

    test1_summary
    test1_description

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>--- TOMTEK UA LLC ---</title>
+</head>
+
+<body>
+
+	<header>
+		
+	</header>
+
+	<main>
+		
+		<p>text 1 >>> html >>> Hello world!</p>
+		
+		<?php
+		echo "text 2 >>> php >>> Hello world!";
+		?>
+		
+	</main>
+
+	<footer>
+		
+	</footer>
+
+</body>
+
+</html>
